2967d47b4661041b81be586e06c0fcb9175e7547
Mark Basmayor <mark.basmayor@gmail.com>
2019-03-26 23:31:50 +0800

    Add powerlevel9k prompt for zsh

--- zshrc/install.sh
+++ zshrc/install.sh
@@ -1,37 +1,5 @@
 #!/bin/bash
 
-### env
-BASH_ALIASES_FILE=~/.bash_aliases
-GITCONFIG_FILE=~/.gitconfig
-PWD=`pwd`
-
-### functions
-warn() {
-    echo "WARNING: $1" >&2
-}
-
-error() {
-    echo "ERROR: $1" >&2
-    exit 1
-}
-
-### begin
-echo "installing dotfiles in: $PWD ..."
-
-#backup exists file/dir
-
-echo "creating backup ..."
-
-for f in $BASH_ALIASES_FILE $GITCONFIG_FILE; do
-    if [ -e "$f" ]; then
-        if [ -e "${f}_bak" ]; then
-            rm -rf ${f}_bak || error "Can't remove exists backup file: ${f}_bak"
-        fi
-        mv $f ${f}_bak || error "Can't rename exists file: $f"
-    fi
-done
-
-# link the files
-echo "Create symbolic links ..."
-ln -sf $PWD/bash_aliases $BASH_ALIASES_FILE
-ln -sf $PWD/gitconfig $GITCONFIG_FILE
+cp themes/* ~/.oh-my-zsh/custom/themes -R
+cp powerlevel9k.zsh ~/.oh-my-zsh/custom/powerlevel9k.zsh
+echo "source ~/.oh-my-zsh/custom/powerlevel9k.zsh" >> ~/.zshrc
